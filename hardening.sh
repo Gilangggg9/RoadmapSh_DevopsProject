fc35d1f4d409aa55215bb834081c2016d0e293fd
Gilangggg9 <90899620+Gilangggg9@users.noreply.github.com>
2025-12-09 21:58:15 +0700

    Create hardening.sh

---
+++ hardening.sh
@@ -0,0 +1,11 @@
+
+//membuat script hardening secara otomatis
+//Script mencakup:
+// Update Dependecy - Update system 
+- Setup Network (IP: 172.31.137.90, IP Gateway: 172.31.137.1, Nameserver: 103.84.10.201 ) 
+- Secure Network (IP Spoofing protection, Block SYN attacks, Disable IPv6, Hide kernel pointers, 
+Enable panic on OOM) 
+- Secure SSHD (disable tunneled clear text passwords, Do not allow empty passwords, log all 
+activity, change SSH default port) 
+- configure firewall (allow only http, https, ssh) 
+- free disk space
